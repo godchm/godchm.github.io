879abdeb76eb1e72dfc4d894e23097251cc10bb8
godchm <choehyeongmin7@gmail.com>
2026-07-10 16:26:25 +0900

    이력서 3차

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-const repository = process.env.GITHUB_REPOSITORY ?? '';
-const [owner = 'username', repo = 'username.github.io'] = repository.split('/');
-const isUserPage = repo === `${owner}.github.io`;
-
 export default defineConfig({
-  site: `https://${owner}.github.io`,
-  base: isUserPage ? '/' : `/${repo}`,
+  site: 'https://godchm.github.io',
 });
